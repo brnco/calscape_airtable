f7d9b356d190edd1a84b43b6ec62eb022a7e4335
Brendan Coates <brendan.emerson.coates@gmail.com>
2022-08-09 12:41:34 -0700

    init

---
+++ calscape_airtable.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+'''
+resources for converting CalScape data for use in Airtable
+'''
+import argparse
+import re
+
+def main():
+    '''
+    do the thing
+    '''
+    print("eyo")
+
+if __name__ == "__main__":
+    main()
